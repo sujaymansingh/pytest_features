f8f54f28399d0abbc3f34dbadab1763f9db504d4
Sujay Mansingh <info@sujaymansingh.com>
2020-01-07 13:50:46 +0000

    Use fixtures to avoid duplication

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,16 @@
+import pytest
+
 import calculator
 
 
-def test_get_sum():
-    numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+@pytest.fixture
+def numbers():
+    return [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def test_get_sum(numbers):
     assert calculator.get_sum(numbers) == 36
 
 
-def test_get_product():
-    numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+def test_get_product(numbers):
     assert calculator.get_product(numbers) == 40320
