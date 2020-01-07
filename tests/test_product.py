07b1fe74fb29227ea11b178a9fa523fa0825ea5f
Sujay Mansingh <info@sujaymansingh.com>
2020-01-07 14:10:50 +0000

    Add more product tests

--- tests/test_product.py
+++ tests/test_product.py
@@ -6,3 +6,13 @@ import calculator
 @pytest.mark.product
 def test_get_product(numbers):
     assert calculator.get_product(numbers) == 40320
+
+
+@pytest.mark.product
+def test_1_times_2():
+    assert calculator.get_product([1, 2]) == 2
+
+
+@pytest.mark.product
+def test_3_times_4():
+    assert calculator.get_product([3, 4]) == 12
