c0f9b6550bae0e47cf5f158cd1ad01ee630aa37d
Sujay Mansingh <info@sujaymansingh.com>
2020-01-07 13:58:50 +0000

    Move fixture into common file: DRY

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.fixture
+def numbers():
+    return [1, 2, 3, 4, 5, 6, 7, 8]
--- tests/test_product.py
+++ tests/test_product.py
@@ -3,11 +3,6 @@ import pytest
 import calculator
 
 
-@pytest.fixture
-def numbers():
-    return [1, 2, 3, 4, 5, 6, 7, 8]
-
-
 @pytest.mark.product
 def test_get_product(numbers):
     assert calculator.get_product(numbers) == 40320
