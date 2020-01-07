acc00aee933339f45382419dd53b740498f83dc4
Sujay Mansingh <info@sujaymansingh.com>
2020-01-07 13:53:13 +0000

    Use marks to control which tests are run

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,9 +8,11 @@ def numbers():
     return [1, 2, 3, 4, 5, 6, 7, 8]
 
 
+@pytest.mark.sum
 def test_get_sum(numbers):
     assert calculator.get_sum(numbers) == 36
 
 
+@pytest.mark.product
 def test_get_product(numbers):
     assert calculator.get_product(numbers) == 40320
