1203f74a742bca2d6c1d9a5c14676fb39b05b2fd
Sujay Mansingh <info@sujaymansingh.com>
2020-01-07 14:04:33 +0000

    Run something before/after each test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,22 @@
+import datetime
+
 import pytest
 
 
 @pytest.fixture
 def numbers():
     return [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+@pytest.fixture(autouse=True)
+def record_times():
+    # Do stuff that needs to be setup at the beginning of each test...
+    with open("logfile.log", "a") as out:
+        out.write(f"Started at {datetime.datetime.utcnow()}\n")
+
+    # Run the actual tests...
+    yield
+
+    # Do anything that needs to be done at the end of each test...
+    with open("logfile.log", "a") as out:
+        out.write(f"Started at {datetime.datetime.utcnow()}\n")
